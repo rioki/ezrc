6a2a3bd96edf3897667afe7b8c9cb3a4d9dea905
Sean Farrell <sean.farrell@rioki.org>
2026-03-07 14:52:19 +0100

    Implements functional beauty fixes to the generated code.
    fixes #8
    fixes #9

--- ezrc-test/resources.h
+++ ezrc-test/resources.h
@@ -6,5 +6,17 @@
 
 namespace test 
 {
-
-     std::string_view get_string_resource(const std::string_view file);}
+    //! Retrieves the content of an embedded resource file as a string view.
+    //!
+    //! This function provides read-only access to files embedded into the binary
+    //! by the ezrc compiler. The returned view points directly into the
+    //! read-only data section and remains valid for the entire program lifetime.
+    //!
+    //! @param file The filename of the resource
+    //!
+    //! @return A non-owning view into the resource data. The view is null-terminated
+    //!         so `.data()` can be used with C-style APIs that expect null-terminated strings.
+    //!
+    //! @throw resource_not_found If no resource with the given @p file name exists.
+    std::string_view get_resource(const std::string_view file);
+}
